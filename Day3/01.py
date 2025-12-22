b7b790cf6e8846efade2f475b55d775a0643326b
Nasir imam <soulpanther4@gmail.com>
2025-12-22 16:58:12 +0530

    Day3 Done

---
+++ Day3/01.py
@@ -0,0 +1,6 @@
+# Check and print the given number is 3 digit or not
+num = 999
+
+val1 = "Three Digit Num" if 100 <= num <= 999 else "Not A Three Dignit Number"
+
+print(val1)
---
+++ Day3/02.py
@@ -0,0 +1,5 @@
+# Print Bigger of two Numbers
+a = 30
+b = 20
+res = a if a > b else b
+print(res)
